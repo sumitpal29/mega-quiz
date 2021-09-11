7bd90d6eb2858469b7cf5a6984d5082e17daa82a
sumitpal29 <sumitwithyou@gmail.com>
2021-09-11 21:01:57 +0530

    server client setup

---
+++ server/constants/constant.status.js
@@ -0,0 +1,9 @@
+const ERROR = 'ERROR';
+const FAILED = 'FAILED';
+const SUCCESS = 'SUCCESS';
+
+export default {
+  ERROR,
+  FAILED,
+  SUCCESS,
+}
--- server/router/index.js
+++ server/router/index.js
@@ -1,26 +1,25 @@
 const express = require('express')
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-const Admin = require('../models/admin')
+const Admin = require('../models/admin');
+const STATUS = require('../constants/constant.status');
 
 router.post('/api/admin-signup', async (req, res) => {
   try {
     const { username, password } = req.body;
     const hashedPassword  = await bcrypt.hash(password, 10);
-    console.log('pass', hashedPassword)
     const response = await Admin.create({
       username,
       password: hashedPassword,
     });
-
-    console.log(response)
-    res.status(201).send({ status: 'SUCCESS', data: {
+    res.status(201).send({ status: STATUS.SUCCESS, data: {
       username
     }});
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
     res.status(400).send({
-      error: 'Admin creation failed'
+      status: STATUS.ERROR,
+      error
     })
   }
 });
